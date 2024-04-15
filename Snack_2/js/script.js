c3da27a76c3e3beaebbd1531c3fc302542d012e5
wmancaniello <wmancaniello@gmail.com>
2024-04-15 17:16:53 +0200

    create array

---
+++ Snack_2/js/script.js
@@ -0,0 +1,21 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+// - Nome
+// - Peso
+// 2) Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+// Array di oggetti
+const biciCorsa = [
+  {
+    nome: "Olmo",
+    peso: 10,
+  },
+  {
+    nome: "Basso",
+    peso: 7,
+  },
+  {
+    nome: "Cipollini",
+    peso: 12,
+  },
+];
