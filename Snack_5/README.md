0c66697c69b0b6e3c3e06298c5b6110c98f12de6
wmancaniello <wmancaniello@gmail.com>
2024-04-16 12:22:27 +0200

    first commit

---
+++ Snack_5/README.md
@@ -0,0 +1,9 @@
+1) Creo l'array delle persone con le proprietà:
+    - Nome
+    - Cognome
+    - Età
+
+2) Creo un array vuoto da riempire
+
+3) Utilizzo forEach per iterare gli oggetti (persone):
+    - Creo la frase e di conseguenza psgo nell'array
---
+++ Snack_5/js/script.js
@@ -0,0 +1,3 @@
+// Crea un array di oggetti che rappresentano delle persone.
+// Ogni persona ha un nome, un cognome e un’età.
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
